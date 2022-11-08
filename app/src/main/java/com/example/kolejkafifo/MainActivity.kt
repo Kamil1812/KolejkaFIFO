4aca4e70f99d7e7dac4a4fae2d9ba81dc097e617
kstajbnz <k.stajkowski@o2.pl>
2022-11-08 09:28:37 +0100

    Zaimplementowanie wartości podanych przez użytkownika i dodania do kolejki

--- app/src/main/java/com/example/kolejkafifo/MainActivity.kt
+++ app/src/main/java/com/example/kolejkafifo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.kolejkafifo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // KAMIL NIEWIADOMSKI 3pr-T5
+
+        findViewById<Button>(R.id.btn_add).setOnClickListener {
+            val value = findViewById<TextInputLayout>(R.id.textInputLayout).toString()
+            val value_num = value.toInt()
+            val tab : Array<Int> = arrayOf()
+        }
     }
 }
