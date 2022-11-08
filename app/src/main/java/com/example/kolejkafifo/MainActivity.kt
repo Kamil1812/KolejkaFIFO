7bc825f45d5a1662d86f50d567359186e33b0d07
kstajbnz <k.stajkowski@o2.pl>
2022-11-08 10:58:39 +0100

    first()

--- app/src/main/java/com/example/kolejkafifo/MainActivity.kt
+++ app/src/main/java/com/example/kolejkafifo/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 import java.util.*
@@ -18,9 +19,41 @@ class MainActivity : AppCompatActivity() {
         val i = 0
 
         val queue: Queue<Int> = LinkedList<Int>()
+        val text = findViewById<EditText>(R.id.myEditText)
+        val output = findViewById<TextView>(R.id.TextView_wynik)
 
         findViewById<Button>(R.id.btn_add).setOnClickListener {
-            queue.add(findViewById<EditText>(R.id.myEditText).text.toString().toInt())
+            queue.add(text.text.toString().toInt())
         }
+
+        findViewById<Button>(R.id.btn_wykonaj).setOnClickListener {
+
+
+            if (findViewById<RadioButton>(R.id.rb_first).isChecked)
+            {
+                output.text = queue.first().toString()
+            }
+
+            if (findViewById<RadioButton>(R.id.rb_last).isChecked)
+            {
+
+            }
+
+            if (findViewById<RadioButton>(R.id.rb_size).isChecked)
+            {
+
+            }
+
+            if (findViewById<RadioButton>(R.id.rb_push_back).isChecked)
+            {
+
+            }
+
+            if (findViewById<RadioButton>(R.id.rb_pop_back).isChecked)
+            {
+
+            }
+        }
+
     }
 }
